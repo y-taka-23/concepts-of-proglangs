4b6a96a2d7bad103c26e8be9fd48412bf6c1f546
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-08-23 02:45:02 +0900

    Define expressions with environments

---
+++ Definitions.v
@@ -0,0 +1,31 @@
+Section Definitions.
+Require Import ZArith.
+Open Scope Z_scope.
+
+(* v in Value ::= i | b (in def 3.1) *)
+Inductive Value : Set := 
+    | VInt  : Z -> Value
+    | VBool : bool -> Value.
+
+(* Variables at p.71 *)
+Inductive Var : Set :=
+    | var : nat -> Var.
+
+(* Environments at p.71 *)
+Inductive Env : Set :=
+    | ENil  : Env 
+    | ECons : Env -> Var -> Value -> Env.
+
+(* Definition at p.71 *)
+Inductive Exp : Set :=
+    | EValue : Value -> Exp
+    | EVar   : Var -> Exp
+    | ELet   : Var -> Exp -> Exp -> Exp
+    | EIf    : Exp -> Exp -> Exp -> Exp
+    | EPlus  : Exp -> Exp -> Exp
+    | EMinus : Exp -> Exp -> Exp
+    | ETimes : Exp -> Exp -> Exp
+    | ELt    : Exp -> Exp -> Exp.
+
+End Definitions.
+
