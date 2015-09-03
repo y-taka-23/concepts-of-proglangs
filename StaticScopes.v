43dbda2ecf9b58f97cb4dd42f9dffb87ca42f428
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-09-04 03:11:38 +0900

    Define translation rules

--- StaticScopes.v
+++ StaticScopes.v
@@ -3,11 +3,8 @@ Require Import Functions.
 Require Import ZArith.
 Open Scope Z_scope.
 
-(* Variables (reused) *)
-Inductive Var : Set :=
-    | VId : nat -> Var.
-
 (* Expressions with de Bruijn indices at p.97 and p.100 *)
+(* The indices are 0-besed, unlike the definition in the text *)
 Inductive DBExp : Set :=
     | DBEValue  : DBValue -> DBExp
     | DBEVar    : nat -> DBExp
@@ -33,6 +30,45 @@ Inductive DBExp : Set :=
 Inductive VarList : Set :=
     | VLNil  : VarList
     | VLCons : VarList -> Var -> VarList.
+
+(* Fig 6.1, 6.2 *)
+Inductive TransTo : VarList -> Exp -> DBExp -> Prop :=
+    | Tr_Int    : forall (X : VarList) (i : Z),
+                  TransTo X (EValue (VInt i)) (DBEValue (DBVInt i))
+    | Tr_Bool   : forall (X : VarList) (b : bool),
+                  TransTo X (EValue (VBool b)) (DBEValue (DBVBool b))
+    | Tr_If     : forall (X : VarList) (e1 e2 e3 : Exp) (d1 d2 d3 : DBExp),
+                  TransTo X e1 d1 -> TransTo X e2 d2 -> TransTo X e3 d3 ->
+                  TransTo X (EIf e1 e2 e3) (DBEIf d1 d2 d3)
+    | Tr_Plus   : forall (X : VarList) (e1 e2 : Exp) (d1 d2 : DBExp),
+                  TransTo X e1 d1 -> TransTo X e2 d2 ->
+                  TransTo X (EPlus e1 e2) (DBEPlus d1 d2)
+    | Tr_Minus  : forall (X : VarList) (e1 e2 : Exp) (d1 d2 : DBExp),
+                  TransTo X e1 d1 -> TransTo X e2 d2 ->
+                  TransTo X (EMinus e1 e2) (DBEMinus d1 d2)
+    | Tr_Times  : forall (X : VarList) (e1 e2 : Exp) (d1 d2 : DBExp),
+                  TransTo X e1 d1 -> TransTo X e2 d2 ->
+                  TransTo X (ETimes e1 e2) (DBETimes d1 d2)
+    | Tr_Lt     : forall (X : VarList) (e1 e2 : Exp) (d1 d2 : DBExp),
+                  TransTo X e1 d1 -> TransTo X e2 d2 ->
+                  TransTo X (ELt e1 e2) (DBELt d1 d2)
+    | Tr_Var1   : forall (X : VarList) (x : Var),
+                  TransTo (VLCons X x) (EVar x) (DBEVar O)
+    | Tr_Var2   : forall (X : VarList) (x y : Var) (n1 : nat),
+                  y <> x -> TransTo X (EVar x) (DBEVar n1) ->
+                  TransTo (VLCons X y) (EVar x) (DBEVar (S n1))
+    | Tr_Let    : forall (X : VarList) (x : Var) (e1 e2 : Exp) (d1 d2 : DBExp),
+                  TransTo X (ELet x e1 e2) (DBELet d1 d2)
+    | Tr_Fun    : forall (X : VarList) (x : Var) (e : Exp) (d : DBExp),
+                  TransTo X (EFun x e) (DBEFun d)
+    | Tr_App    : forall (X : VarList) (e1 e2 : Exp) (d1 d2 : DBExp),
+                  TransTo X e1 d1 -> TransTo X e2 d2 ->
+                  TransTo X (EApp e1 e2) (DBEApp d1 d2)
+    | Tr_LetRec : forall (X : VarList) (x y : Var)
+                         (e1 e2 : Exp) (d1 d2 : DBExp),
+                  TransTo (VLCons (VLCons X x) y) e1 d1 ->
+                  TransTo (VLCons X x) e2 d2 ->
+                  TransTo X (ELetRec x y e1 e2) (DBELetRec d1 d2).
 
 End StaticScopes.
 
