edc1a0adc32f9c2b1cf84c3f636473d777644c69
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-09-21 14:56:16 +0900

    Define the typing rules

--- SimpleTypeSystem.v
+++ SimpleTypeSystem.v
@@ -3,5 +3,72 @@ Require Import PatternMatching.
 Require Import ZArith.
 Open Scope Z_scope.
 
+(* Types at p.125 *)
+Inductive Types : Set :=
+    | TBool : Types
+    | TInt  : Types
+    | TFun  : Types -> Types -> Types
+    | TList : Types -> Types.
+
+(* Type environments at p.125 *)
+Inductive TEnv : Set :=
+    | TEEmpty : TEnv
+    | TEBind  : TEnv -> Var -> Types -> TEnv.
+
+(* Type lookup at p.124 *)
+Inductive has_type : TEnv -> Var -> Types -> Prop :=
+    | HT_Bind1 : forall (C : TEnv) (x : Var) (t : Types),
+                 has_type (TEBind C x t) x t
+    | HT_Bind2 : forall (C : TEnv) (x y : Var) (t t0 : Types),
+                 has_type C x t -> y <> x ->
+                 has_type (TEBind C y t0) x t.
+
+(* Fig 8.1 and 8.2 *)
+Inductive Typable : TEnv -> Exp -> Types -> Prop :=
+    | T_Int    : forall (C : TEnv) (i : Z),
+                 Typable C (EInt i) TInt
+    | T_Bool   : forall (C : TEnv) (b : bool),
+                 Typable C (EBool b) TBool
+    | T_If     : forall (C : TEnv) (e1 e2 e3 : Exp) (t : Types),
+                 Typable C e1 TBool -> Typable C e2 t -> Typable C e3 t ->
+                 Typable C (EIf e1 e2 e3) t
+    | T_Plus   : forall (C : TEnv) (e1 e2 : Exp),
+                 Typable C e1 TInt -> Typable C e2 TInt ->
+                 Typable C (EPlus e1 e2) TInt
+    | T_Minus  : forall (C : TEnv) (e1 e2 : Exp),
+                 Typable C e1 TInt -> Typable C e2 TInt ->
+                 Typable C (EMinus e1 e2) TInt
+    | T_Times  : forall (C : TEnv) (e1 e2 : Exp),
+                 Typable C e1 TInt -> Typable C e2 TInt ->
+                 Typable C (ETimes e1 e2) TInt
+    | T_Lt     : forall (C : TEnv) (e1 e2 : Exp),
+                 Typable C e1 TInt -> Typable C e2 TInt ->
+                 Typable C (ELt e1 e2) TBool
+    | T_Var    : forall (C : TEnv) (x : Var) (t : Types),
+                 has_type C x t ->
+                 Typable C (EVar x) t
+    | T_Let    : forall (C : TEnv) (e1 e2 : Exp) (x : Var) (t1 t2 : Types),
+                 Typable C e1 t1 -> Typable (TEBind C x t1) e2 t2 ->
+                 Typable C (ELet x e1 e2) t2
+    | T_Fun    : forall (C : TEnv) (e : Exp) (x : Var) (t1 t2 : Types),
+                 Typable (TEBind C x t1) e t2 ->
+                 Typable C (EFun x e) (TFun t1 t2)
+    | T_App    : forall (C : TEnv) (e1 e2 : Exp) (t1 t2 : Types),
+                 Typable C e1 (TFun t1 t2) -> Typable C e2 t1 ->
+                 Typable C (EApp e1 e2) t2
+    | T_LetRec : forall (C : TEnv) (e1 e2 : Exp) (x y : Var) (t t1 t2 : Types),
+                 Typable (TEBind (TEBind C x (TFun t1 t2)) y t1) e1 t2 ->
+                 Typable (TEBind C x (TFun t1 t2)) e2 t ->
+                 Typable C (ELetRec x y e1 e2) t
+    | T_Nil    : forall (C : TEnv) (t : Types),
+                 Typable C ENil (TList t)
+    | T_Cons   : forall (C : TEnv) (e1 e2 : Exp) (t : Types),
+                 Typable C e1 t -> Typable C e2 (TList t) ->
+                 Typable C (ECons e1 e2) (TList t)
+    | T_Match  : forall (C : TEnv) (e1 e2 e3 : Exp) (x y : Var) (t t' : Types),
+                 Typable C e1 (TList t') -> Typable C e2 t ->
+                 Typable (TEBind (TEBind C x t') y (TList t')) e3 t ->
+                 Typable C (EMatch e1 e2 x y e3) t.
+
 End SimpleTypeSystem.
 
